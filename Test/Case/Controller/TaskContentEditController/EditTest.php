9420caa2f09088f5d9aeb074c98d1947689414a5
中島 正平 <nakajimashouhei@gmail.com>
2017-05-27 12:28:09 +0900

    calendar_eventsのFixture追加

--- Test/Case/Controller/TaskContentEditController/EditTest.php
+++ Test/Case/Controller/TaskContentEditController/EditTest.php
@@ -25,6 +25,7 @@ class TaskContentEditControllerEditTest extends WorkflowControllerEditTest {
  * @var array
  */
 	public $fixtures = array(
+		'plugin.calendars.calendar_event',
 		'plugin.categories.category',
 		'plugin.categories.category_order',
 		'plugin.categories.categories_language',
